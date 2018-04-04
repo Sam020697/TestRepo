220b82c738035133fddad23139c75bc3f66b3610
varunsharma8055 <varun.sharma8055@gmail.com>
2018-04-04 19:27:23 +0530

    New
    Blah Blah

--- partials/home.php
+++ partials/home.php
@@ -4,7 +4,7 @@
   <body>
     <?php include'navbar.html';?>
     <div class = "rows abc">
-      <img src="images/home.jpg" class="img-responsive"></img>
+      <img src="images/yolo.jpg" class="img-responsive"></img>
     </div>
   </body>
 </html>
